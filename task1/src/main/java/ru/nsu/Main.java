58632b94ec1cba8ca42f3e802d7eae5ac2204035
ValeriaPetrova <73832354+ValeriaPetrova@users.noreply.github.com>
2021-09-23 16:48:40 +0700

    Update Main.java

--- task1/src/main/java/ru/nsu/Main.java
+++ task1/src/main/java/ru/nsu/Main.java
@@ -5,8 +5,11 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String group = args[0];
-        Checker checker= new Checker(group);
+        if (args.length < 0) {
+            System.out.println("Error: empty input. You should enter the group ID");
+            System.exit(1);
+        }
+        Checker checker= new Checker(args[0]);
         checker.run();
     }
 }
