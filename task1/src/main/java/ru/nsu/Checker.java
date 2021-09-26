51e9b9f46f06c188433d918f526f5411839b9aec
ValeriaPetrova <73832354+ValeriaPetrova@users.noreply.github.com>
2021-09-26 20:11:34 +0700

    adding counting live copies; 1 UUID

--- task1/src/main/java/ru/nsu/Checker.java
+++ task1/src/main/java/ru/nsu/Checker.java
@@ -5,68 +5,80 @@ import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.SocketTimeoutException;
+import java.util.HashMap;
+import java.util.Set;
 import java.util.UUID;
 
 public class Checker {
     private String group;
 
     private static final int PORT = 8000;
-    private static final String MULTICAST_MESSAGE = "Hello world!";
     private static final int LENGTH = 16;
-    private static final int BUF_SIZE = 2 * LENGTH;
     private static final int MAX_ITER = 10000;
     private static final int TIMEOUT = 1000;
+
+    private HashMap<UUID, Long> copies = new HashMap<>();
 
     public Checker(String group) {
         this.group = group;
     }
 
-    public void run() throws IOException {
-        UUID uuidMulticastMessageSend = UUID.nameUUIDFromBytes(MULTICAST_MESSAGE.getBytes());
-        byte[] firstSend = UuidHelper.getBytesFromUUID(uuidMulticastMessageSend);
-        UUID uuidProgramSend = UUID.randomUUID();
-        byte[] secondSend = UuidHelper.getBytesFromUUID(uuidProgramSend);
+    public void checkPackets(UUID uuidMulticastMessageSend, UUID uuidMulticastMessageReceive) {
+        if (copies.containsKey(uuidMulticastMessageReceive)) {
+            copies.replace(
+                    uuidMulticastMessageReceive,
+                    copies.get(uuidMulticastMessageReceive),
+                    System.currentTimeMillis()
+            );
+        }
 
-        byte[] bufSender = new byte[BUF_SIZE];
-        System.arraycopy(firstSend, 0, bufSender, 0, firstSend.length);
-        System.arraycopy(secondSend, 0, bufSender, firstSend.length, secondSend.length);
+        if (uuidMulticastMessageSend.equals(uuidMulticastMessageReceive)) {
+            System.out.println("Found myself");
+        } else {
+            copies.put(uuidMulticastMessageReceive, System.currentTimeMillis());
+            System.out.println("Copy found: " + uuidMulticastMessageReceive.toString());
+        }
+    }
+
+    public void run() throws IOException {
+        UUID uuidMulticastMessageSend = UUID.randomUUID();
+        byte[] sendBuf = UuidHelper.getBytesFromUUID(uuidMulticastMessageSend);
 
         MulticastSocket multicastSocket = new MulticastSocket(PORT);
         multicastSocket.setSoTimeout(TIMEOUT);
         multicastSocket.joinGroup(InetAddress.getByName(group));
 
         DatagramPacket datagramPacketSend = new DatagramPacket(
-                bufSender,
-                bufSender.length,
+                sendBuf,
+                sendBuf.length,
                 InetAddress.getByName(group),
                 PORT
         );
 
-        byte[] bufReceiver = new byte[BUF_SIZE];
+        byte[] receiveBuf = new byte[LENGTH];
         DatagramPacket datagramPacketReceive = new DatagramPacket(
-                bufReceiver,
-                bufReceiver.length
+                receiveBuf,
+                receiveBuf.length
         );
 
         for (int iter = 0; iter < MAX_ITER; iter++) {
             multicastSocket.send(datagramPacketSend);
-            multicastSocket.receive(datagramPacketReceive);
-            bufReceiver = datagramPacketReceive.getData();
-            byte[] firstReceive = new byte[LENGTH];
-            byte[] secondReceive = new byte[LENGTH];
-            System.arraycopy(bufReceiver, 0, firstReceive, 0, firstReceive.length);
-            System.arraycopy(bufReceiver, firstReceive.length, secondReceive, 0, secondReceive.length);
 
-            UUID uuidMulticastMessageReceive = UuidHelper.getUUIDFromBytes(firstReceive);
-            UUID uuidProgramReceive = UuidHelper.getUUIDFromBytes(secondReceive);
-            
-            if (uuidMulticastMessageSend.equals(uuidMulticastMessageReceive) && !uuidProgramSend.equals(uuidProgramReceive)) {
-                System.out.println("------Copy found------");
-            } else {
-                System.out.println("Copy not found");
+            try {
+                multicastSocket.receive(datagramPacketReceive);
+            } catch (SocketTimeoutException e) {
+                continue;
             }
+
+            receiveBuf = datagramPacketReceive.getData();
+            UUID uuidMulticastMessageReceive = UuidHelper.getUUIDFromBytes(receiveBuf);
+
+            checkPackets(uuidMulticastMessageSend, uuidMulticastMessageReceive);
         }
         multicastSocket.leaveGroup(InetAddress.getByName(group));
         multicastSocket.close();
+        System.out.println("Number of clones found: " + copies.size());
     }
 }
+
+
