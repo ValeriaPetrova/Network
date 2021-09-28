1b5b72c1190bb32b9c5124cdf933ffec2821c595
ValeriaPetrova <v.petrova@g.nsu.ru>
2021-09-28 18:28:13 +0700

    create 2 threads; fix gitignore

--- task1/src/main/java/ru/nsu/Checker.java
+++ task1/src/main/java/ru/nsu/Checker.java
@@ -1,37 +1,24 @@
 package ru.nsu;
 
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.InetAddress;
-import java.net.MulticastSocket;
-import java.net.SocketTimeoutException;
 import java.util.HashMap;
-import java.util.Set;
 import java.util.UUID;
 
 public class Checker {
-    private String group;
+    public HashMap<UUID, Long> copies = new HashMap<>();
 
-    private static final int PORT = 8000;
-    private static final int LENGTH = 16;
-    private static final int MAX_ITER = 10000;
-    private static final int TIMEOUT = 1000;
-
-    private HashMap<UUID, Long> copies = new HashMap<>();
-
-    public Checker(String group) {
-        this.group = group;
+    public void addElemToMap(UUID uuid, long value) {
+        if (copies.containsKey(uuid)) {
+            copies.replace(
+                    uuid,
+                    copies.get(uuid),
+                    value
+            );
+            return;
+        }
+        copies.put(uuid, value);
     }
 
     public void checkPackets(UUID uuidMulticastMessageSend, UUID uuidMulticastMessageReceive) {
-        if (copies.containsKey(uuidMulticastMessageReceive)) {
-            copies.replace(
-                    uuidMulticastMessageReceive,
-                    copies.get(uuidMulticastMessageReceive),
-                    System.currentTimeMillis()
-            );
-        }
-
         if (uuidMulticastMessageSend.equals(uuidMulticastMessageReceive)) {
             System.out.println("Found myself");
         } else {
@@ -40,45 +27,6 @@ public class Checker {
         }
     }
 
-    public void run() throws IOException {
-        UUID uuidMulticastMessageSend = UUID.randomUUID();
-        byte[] sendBuf = UuidHelper.getBytesFromUUID(uuidMulticastMessageSend);
-
-        MulticastSocket multicastSocket = new MulticastSocket(PORT);
-        multicastSocket.setSoTimeout(TIMEOUT);
-        multicastSocket.joinGroup(InetAddress.getByName(group));
-
-        DatagramPacket datagramPacketSend = new DatagramPacket(
-                sendBuf,
-                sendBuf.length,
-                InetAddress.getByName(group),
-                PORT
-        );
-
-        byte[] receiveBuf = new byte[LENGTH];
-        DatagramPacket datagramPacketReceive = new DatagramPacket(
-                receiveBuf,
-                receiveBuf.length
-        );
-
-        for (int iter = 0; iter < MAX_ITER; iter++) {
-            multicastSocket.send(datagramPacketSend);
-
-            try {
-                multicastSocket.receive(datagramPacketReceive);
-            } catch (SocketTimeoutException e) {
-                continue;
-            }
-
-            receiveBuf = datagramPacketReceive.getData();
-            UUID uuidMulticastMessageReceive = UuidHelper.getUUIDFromBytes(receiveBuf);
-
-            checkPackets(uuidMulticastMessageSend, uuidMulticastMessageReceive);
-        }
-        multicastSocket.leaveGroup(InetAddress.getByName(group));
-        multicastSocket.close();
-        System.out.println("Number of clones found: " + copies.size());
-    }
 }
 
 
--- task1/src/main/java/ru/nsu/Main.java
+++ task1/src/main/java/ru/nsu/Main.java
@@ -9,7 +9,10 @@ public class Main {
             System.out.println("Error: empty input. You should enter the group ID");
             System.exit(1);
         }
-        Checker checker = new Checker(args[0]);
-        checker.run();
+        Checker checker = new Checker();
+        MulticastSender multicastSender = new MulticastSender(args[0], checker);
+        multicastSender.start();
+        MulticastReceiver multicastReceiver = new MulticastReceiver(args[0], checker);
+        multicastReceiver.start();
     }
 }
